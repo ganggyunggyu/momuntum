3839e228ffa967360bda90f58f46d19cc41d9a2e
GyungGyu Gang <qwzx16@naver.com>
2022-05-09 22:45:43 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const calculator = {
+    add(a,b){
+        console.log(a+b);
+        },
+    minus(a,b){
+        console.log(a-b);
+    },
+    power(a,b){
+        console.log(a**b);
+    },
+    div(a,b){
+        console.log(a/b);
+    }
+};
+
+calculator.add(1,1);
+calculator.minus(3,2);
+calculator.power(3,3);
+calculator.div(10,2);
