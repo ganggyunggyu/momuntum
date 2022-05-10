1ba251e9783f6e5804fccac73862a7466ecd3270
GyungGyu Gang <qwzx16@naver.com>
2022-05-10 23:42:02 +0900

    drinkAgeTest commit

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
-const calculator = {
-    add: function(a,b){
-        return a+b;
-    },
-    minus: function(a,b){
-        return a-b;
-    },
-    multi: function(a,b){
-        return a*b;
-    },
-    div: function(a,b){
-        return a/b;
-    },
-    power: function(a,b){
-        return a**b;
-    }
-};
+const age = parseInt(prompt("너 몇살이야!"));
 
-AddResult = calculator.add(10,10);
-MinusResult = calculator.minus(AddResult,5);
-MultiResult = calculator.multi(MinusResult,2);
-DivResult = calculator.div(MultiResult,3);
-PowerResult = calculator.power(DivResult,10);
-
-console.log(AddResult);
-console.log(MinusResult);
-console.log(MultiResult);
-console.log(DivResult);
-console.log(PowerResult);
-
-console.log("무야호")
+if(isNaN(age)) {
+    alert("나이를 똑바로 입력해줘잉");
+}else if(age<20){
+    alert("음주 금지!");
+}else if(age>19 & age<50){
+    alert("음주 허락!");
+}else if(age>49){
+    alert("건강을 생각해요 가족을 생각해요...");
+}
